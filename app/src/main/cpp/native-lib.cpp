80240c17f059e45a590a0b6e0bfa70f89c3c6c32
zhaikn <857458455@qq.com>
2019-04-03 11:56:30 +0800

    添加扫码跳转逻辑；jni获取mac地址方法

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,72 @@
 #include <jni.h>
 #include <string>
 
+#define BYTE unsigned char
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_tv_newtv_screening_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+jstring charTojstring(JNIEnv* env, const char* pat){
+//定义java String类 strClass
+    jclass strClass = (env)->FindClass("java/lang/String");
+//获取String(byte[],String)的构造器,用于将本地byte[]数组转换为一个新String
+    jmethodID ctorID = (env)->GetMethodID(strClass, "<init>", "([BLjava/lang/String;)V");
+//建立byte数组
+    jbyteArray bytes = (env)->NewByteArray(strlen(pat));
+//将char* 转换为byte数组
+    (env)->SetByteArrayRegion(bytes, 0, strlen(pat), (jbyte*) pat);
+// 设置String, 保存语言类型,用于byte数组转换至String时的参数
+    jstring encoding = (env)->NewStringUTF("GB2312");
+//将byte数组转换为java String,并输出
+    return (jstring) (env)->NewObject(strClass, ctorID, bytes, encoding);
+}
+
+
+
+//jni获取mac地址
+jstring getMacAddress(JNIEnv* env){
+//通过JNI找到java中的NetworkInterface类
+    jclass cls_networkInterface = env->FindClass("java/net/NetworkInterface");
+    if (cls_networkInterface == 0) {
+        return env->NewStringUTF("");
+    }
+//找到getByName方法
+    jmethodID jmethodID1 = env->GetStaticMethodID(cls_networkInterface, "getByName", "(Ljava/lang/String;)Ljava/net/NetworkInterface;");
+    if (jmethodID1 == 0)
+        return env->NewStringUTF("");
+    std::string ss = "wlan0";
+    jstring jss2 = env->NewStringUTF(ss.c_str());
+//调用getByname方法返回NetworkInterface的实例
+    jobject jobject1 = env->CallStaticObjectMethod(cls_networkInterface, jmethodID1, jss2);
+//找到getHardAddress方法
+    jmethodID getHardwareAddress = env->GetMethodID(cls_networkInterface, "getHardwareAddress", "()[B");
+    if (getHardwareAddress == 0)
+        return env->NewStringUTF("");
+//调用getHardAddress方法获取MAC地址的byte[]数组
+    jbyteArray jbyte1 = (jbyteArray)env->CallObjectMethod(jobject1, getHardwareAddress);
+//下面一些列流程就是讲byte[]数组转换成char类型字符在转换成字符串
+    jbyte * olddata = (jbyte*)env->GetByteArrayElements(jbyte1, 0);
+    jsize  oldsize = env->GetArrayLength(jbyte1);
+// BYTE定义为  #define BYTE unsigned char
+    BYTE* bytearr = (BYTE*)olddata;
+    int len = (int)oldsize;
+
+    char* data = (char*)env->GetByteArrayElements(jbyte1, 0);
+    char *temp = new char[len*2 + 1];
+    memset(temp,0,len*2 +1);
+    for (int i = 0; i < len; i++) {
+        char * buffer = new char[2];
+        memset(buffer,2,0);
+        sprintf(buffer, "%02X", data[i]);
+        memcpy(temp+i*2, buffer, 2);
+        delete[] (buffer);
+    }
+    jstring jMac = charTojstring(env, temp);
+    delete[] temp;
+    return jMac;
 }
--- scanning_client/src/main/java/com/fensite/scanning/client/H5ScreenActivity.java
+++ scanning_client/src/main/java/com/fensite/scanning/client/H5ScreenActivity.java
@@ -1,9 +1,12 @@
 package com.fensite.scanning.client;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class H5ScreenActivity extends AppCompatActivity {
     private static String TAG = "screening_h5";
@@ -20,6 +23,35 @@ public class H5ScreenActivity extends AppCompatActivity {
         }
         Log.i("screening_client","param:" + sData);
         WebView wView = (WebView)findViewById(R.id.h5_screen_ui);
+
+        WebViewClient webViewClient = new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView wv, String url) {
+                if(url == null) return false;
+
+                try {
+                    if(url.startsWith("weixin://") //微信
+                            || url.startsWith("alipays://") //支付宝
+                            || url.startsWith("mailto://") //邮件
+                            || url.startsWith("tel://")//电话
+                            || url.startsWith("dianping://")//大众点评
+                            || url.startsWith("js2cmd://") //影音投屏
+                        //其他自定义的scheme
+                            ) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        startActivity(intent);
+                        return true;
+                    }
+                } catch (Exception e) { //防止crash (如果手机上没有安装处理某个scheme开头的url的APP, 会导致crash)
+                    return true;//没有安装该app时，返回true，表示拦截自定义链接，但不跳转，避免弹出上面的错误页面
+                }
+
+                //处理http和https开头的url
+                wv.loadUrl(url);
+                return true;
+            }
+        };
+        wView.setWebViewClient(webViewClient);
         wView.loadUrl(sData);
     }
 }
